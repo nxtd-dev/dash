74339278da812c15abb91eb49821b8ac2792262e
LightHostingFree <141650265+LightHostingFree@users.noreply.github.com>
2025-10-17 18:01:45 +0530

    update

--- app/config/email.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['protocol'] = 'smtp';
-$config['mailtype'] = 'html';
-$config['wordwrap'] = TRUE;
-$config['wrapchars'] = 998;
-$config['smtp_timeout'] = 300;
